d02fca45bf99a6800f25215f69f1d52868e5a307
finnickyfinn <finncastell04@gmail.com>
2024-04-12 23:07:32 -0400

    More code, currently trying to fix daisyui themes

---
+++ my-app/src/Pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Main(){
+    return(
+        <div>
+            <html data-theme="cupcake"></html>
+            <b>Hello</b>
+        </div>
+    )
+}
+
+
+export default Main
