2be32489e3b4d6499f3263037bb9824ee55e16cf
finnickyfinn <finncastell04@gmail.com>
2024-04-13 22:28:19 -0400

    Finished basic services page, need to add link for booking

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -8,15 +8,17 @@ import BaseTransFull from '../Assets/BaseTransFull.png'
 function Header() {
   return (
     <>
-    <div className="navbar bg-base-300">
+    <div className="navbar bg-white">
     <div className="navbar-start">
       <div className="dropdown">
         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </div>
         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-          <li><a>Item 1</a></li>
-          <li><a>Item 3</a></li>
+          <li><Link to ='/'>Home</Link></li>
+          <li><Link to ='/About'>About Us</Link></li>
+          <li><Link to ='/Services'>Services</Link></li>
+          <li><a>Contact Us</a></li>
         </ul>
       </div>
       <Link to='/'className="btn w-52 h-24 btn-ghost"><img className="w-full h-full" src= {BaseTransFull}></img></Link>
--- my-app/src/Pages/Services.js
+++ my-app/src/Pages/Services.js
@@ -1,8 +1,140 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import SpaHero from '../Assets/SpaHero.jpeg'
+import BodyCon from '../Assets/BodyCon.jpeg'
+import FemWell from '../Assets/FemWell.webp'
+import IVTherapy from '../Assets/IVTherapy.jpeg'
+import Laser from '../Assets/Laser.jpeg'
+import SkinTreat from '../Assets/SkinTreat.jpeg'
+import TruUU from '../Assets/TruUU.webp'
+import LabExam from '../Assets/LabEx.webp'
+import Inject from '../Assets/Inject.webp'
+import SkinCare from '../Assets/SkinCare.jpeg'
+import WeightLoss from '../Assets/WeightLoss.avif'
 
 function Services() {
   return (
-    <div>Services</div>
+    <>
+     <div className="hero h-80 " style={{backgroundImage: `url(${SpaHero})` }}>
+        <div className="hero-overlay bg-opacity-40"></div>
+        <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">Services</h1>
+                <p className="mb-5"></p>
+            </div>
+        </div>
+    </div>
+    <div className="flex bg-white py-3 flex-col w-full">
+        <div className="divider">Schedule today and find the service for you.</div>
+    </div>
+    <div class="grid bg-white py-4 px-24 grid-cols-3 gap-8">
+        <div className="card flex-grow-0 hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
+            <figure className="px-10 pt-10">
+            <img src={BodyCon} alt="Shoes" className="rounded-xl" />
+            </figure>
+        <div className="card-body flex-grow-0 items-center text-center">
+            <div className="card-actions">
+            <button className="btn btn-base">Body Contour</button>
+            </div>
+        </div>
+        </div>
+        <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
+            <figure className="px-10 pt-10">
+            <img src={Laser} alt="Shoes" className="rounded-xl" />
+            </figure>
+        <div className="card-body flex-grow-0 items-center text-center">
+            <div className="card-actions">
+            <button className="btn btn-base">Laser Hair Removal</button>
+            </div>
+        </div>
+        </div>
+        <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
+            <figure className="px-10 pt-10">
+            <img src={SkinTreat} alt="Shoes" className="rounded-xl w-72 h-52" />
+            </figure>
+        <div className="card-body flex-grow-0 items-center text-center">
+            <div className="card-actions">
+            <button className="btn btn-base">Skin Treatments</button>
+            </div>
+        </div>
+        </div>
+        <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
+            <figure className="px-10 pt-10">
+            <img src={FemWell} alt="Shoes" className="rounded-xl" />
+            </figure>
+        <div className="card-body flex-grow-0 items-center text-center">
+            <div className="card-actions">
+            <button className="btn btn-base">Feminine Wellness</button>
+            </div>
+        </div>
+        </div>
+        <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
+            <figure className="px-10 pt-10">
+            <img src={IVTherapy} alt="Shoes" className="rounded-xl h-52" />
+            </figure>
+        <div className="card-body flex-grow-0 items-center text-center">
+            <div className="card-actions">
+            <button className="btn btn-base">IV Therapy</button>
+            </div>
+        </div>
+        </div>
+        <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
+            <figure className="px-10 pt-10">
+            <img src={TruUU} alt="Shoes" className="rounded-xl" />
+            </figure>
+        <div className="card-body flex-grow-0 items-center text-center">
+            <div className="card-actions">
+            <button className="btn btn-base">True U Weight Loss</button>
+            </div>
+        </div>
+        </div>
+        <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
+            <figure className="px-10 pt-10">
+            <img src={LabExam} alt="Shoes" className="rounded-xl h-52" />
+            </figure>
+        <div className="card-body flex-grow-0 items-center text-center">
+            <div className="card-actions">
+            <button className="btn btn-base">Lab Exams</button>
+            </div>
+        </div>
+        </div>
+        <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
+            <figure className="px-10 pt-10">
+            <img src={Inject} alt="Shoes" className="rounded-xl" />
+            </figure>
+        <div className="card-body flex-grow-0 items-center text-center">
+            <div className="card-actions">
+            <button className="btn btn-base">Injections</button>
+            </div>
+        </div>
+        </div>
+        <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
+            <figure className="px-10 pt-10">
+            <img src={SkinCare} alt="Shoes" className="rounded-xl" />
+            </figure>
+        <div className="card-body flex-grow-0 items-center text-center">
+            <div className="card-actions">
+            <button className="btn btn-base">Skin Care</button>
+            </div>
+        </div>
+        </div>
+        <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
+            <figure className="px-10 pt-10">
+            <img src={WeightLoss} alt="Shoes" className="rounded-xl" />
+            </figure>
+        <div className="card-body flex-grow-0 items-center text-center">
+            <div className="card-actions">
+            <button className="btn btn-base">Wellness Weight Loss</button>
+            </div>
+        </div>
+        </div>
+        
+    </div>
+
+
+
+    
+    </>
   )
 }
 
