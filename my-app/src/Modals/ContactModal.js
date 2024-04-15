5e717eef7c54bcff628a6920b4b57bd4e7602962
finnickyfinn <finncastell04@gmail.com>
2024-04-14 21:04:25 -0400

    Sorta finsihed product, will be reevamped soon

--- my-app/src/Modals/ContactModal.js
+++ my-app/src/Modals/ContactModal.js
@@ -13,15 +13,14 @@ function ContactModal({ isOpen, closeModal }) {
         <div className="fixed inset-0 z-50 overflow-auto flex items-center justify-center">
           <div className="absolute inset-0 bg-black bg-opacity-50"></div>
           <div className="relative bg-white w-9/12 p-6 rounded-lg">
-            <button onClick={closeModal} className="absolute top-0 right-0 m-4 btn-circle btn-outline">X
-            </button>
-            <div className="mt-4 p-4 border-solid">
+            <button onClick={closeModal} className="absolute top-0 right-0 m-4 btn-circle btn-outline">X</button>
+            {/* <div className="mt-4 p-4 border-solid"> */}
             
-                <div className="card lg:card-side bg-base-100 shadow-xl">
-                    <figure><img src={MedSpaBuild} alt="Album"/></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Get In Touch With Us</h2>
-                        <ul className='pl-2'>
+              <div className="hero bg-base-200">
+                <div className="hero-content  lg:flex-row-reverse">
+                  <div className="text-center justify-end lg:text-left">
+                    <h1 className="text-3xl pl-4 font-bold">Contact Us!</h1>
+                    <ul className='pl-2'>
 
                             <li><a href="mailto:Hello@trueumedspa.com " className='text-lg pl-0 hover:text-white' >Hello@trueumedspa.com</a></li>
                             <li><a href="tel:+14074126080" className='text-lg pl-0 hover:text-white'>407-412-6080</a></li>
@@ -29,12 +28,32 @@ function ContactModal({ isOpen, closeModal }) {
                             <li className='hover:text-white text-lg'>Mon-Fri: 10am-6pm</li>
                             <li className='hover:text-white text-lg'>Sat: Closed</li>
                             <li className='hover:text-white text-lg'>Sun: Closed</li>                   
-                        </ul>                        
-                    </div>
+                        </ul>          
+                  </div>
+                  <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" className="input input-bordered" required />
+                      </div>
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" placeholder="Name" className="input input-bordered" required />
+                      </div>
+                      <div className="form-control mt-6">
+                        <button className="btn btn-primary">Submit</button>
+                      </div>
+                    </form>
+                  </div>
                 </div>
+              </div>
             </div>
           </div>
-        </div>
+        // </div>
       )}
     </>
   );
--- my-app/src/Pages/Services.js
+++ my-app/src/Pages/Services.js
@@ -42,8 +42,8 @@ function Services() {
                             {/* if there is a button in form, it will close the modal */}
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
-                        <h3 className="font-bold text-lg">Hi!</h3>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        <h3 className="font-bold text-lg">Body Contour</h3>
+                        <p className="py-4"> Inmode Tone X5 $1800 <br/> Ultra Cavitation $99 <br/> True Cryolipo $240 <br/> Morpheus 8 body $799 per  session ($2149 for 3 sessions)</p>
                     </div>
                 </dialog>
             </div>
@@ -62,8 +62,11 @@ function Services() {
                             {/* if there is a button in form, it will close the modal */}
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
-                        <h3 className="font-bold text-lg">Hello!</h3>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        <h3 className="font-bold text-lg">Laser Hair Removal</h3>
+                        <h1 className='italic'>Prices May Vary Depending On Area</h1>
+                        <p className="py-4"><b>Small Areas:</b> chin, upper or lower lip, side burns, nose, fingers, toes, cheeks, or stomach strip <br/><br/>
+                                    <b>Medium Areas</b>: underarms, bikini line, hands, abdomen, or (lip & chin) <br/><br/><b> Large Areas:</b> upper legs, lower legs, inner thighs, upper arms, lower arms, upper back, lower back, full neck, or full face <br/><br/><b>XL Areas:</b> full back, abdomen & chest, full legs, full arms, Brazilian, or full butt.
+                        </p>
                     </div>
                 </dialog>
             </div>
@@ -82,8 +85,8 @@ function Services() {
                             {/* if there is a button in form, it will close the modal */}
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
-                        <h3 className="font-bold text-lg">What's Up!</h3>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        <h3 className="font-bold text-lg">Skin Treatments</h3>
+                        <p className="py-4">Botox $10.99 reg tox/ <br/> Juvederm $550/syr</p>
                     </div>
                 </dialog>
             </div>
@@ -91,7 +94,7 @@ function Services() {
         </div>
         <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
             <figure className="px-10 pt-10">
-            <img src={FemWell} alt="Feminine Wellness" className="rounded-xl" />
+            <img src={FemWell} alt="Feminine Wellness" className="rounded-xl w-72 h-52" />
             </figure>
         <div className="card-body flex-grow-0 items-center text-center">
             <div className="card-actions">
@@ -102,8 +105,8 @@ function Services() {
                             {/* if there is a button in form, it will close the modal */}
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
-                        <h3 className="font-bold text-lg">Oh!</h3>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        <h3 className="font-bold text-lg">Feminine Wellness</h3>
+                        <p className="py-4">1 - Inmode Vtone $400/session (6 sessions $1750)<br/>2 - Inmode Forma V $500 (3 sessions $1250)<br/>3 - Morpheus 8 vaginal rejuventation 3 sessions $3500</p>
                     </div>
                 </dialog>
             </div>
@@ -111,19 +114,19 @@ function Services() {
         </div>
         <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
             <figure className="px-10 pt-10">
-            <img src={IVTherapy} alt="IV Therapy" className="rounded-xl h-52" />
+            <img src={IVTherapy} alt="IV Therapy" className="rounded-xl w-72 h-52" />
             </figure>
         <div className="card-body flex-grow-0 items-center text-center">
             <div className="card-actions">
-            <button className="btn" onClick={()=>document.getElementById('my_modal_5').showModal()}>IVTherapy</button>
+            <button className="btn" onClick={()=>document.getElementById('my_modal_5').showModal()}>IV Therapy</button>
                 <dialog id="my_modal_5" className="modal">
                     <div className="modal-box">
                         <form method="dialog">
                             {/* if there is a button in form, it will close the modal */}
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
-                        <h3 className="font-bold text-lg">Welp!</h3>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        <h3 className="font-bold text-lg">IV Therapy Options</h3>
+                        <p className="py-4">1 - Performance blend $189 <br/> 2 - True U beauty blend $189 <br/> 3 - True U immunity blend $175 <br/> 4 - True U anti stress blend $189 5-Iron IV $247</p>
                     </div>
                 </dialog>
             </div>
@@ -131,7 +134,7 @@ function Services() {
         </div>
         <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
             <figure className="px-10 pt-10">
-            <img src={TruUU} alt="True U Weight Loss" className="rounded-xl" />
+            <img src={TruUU} alt="True U Weight Loss" className="rounded-xl w-72 h-52" />
             </figure>
         <div className="card-body flex-grow-0 items-center text-center">
             <div className="card-actions">
@@ -142,8 +145,8 @@ function Services() {
                             {/* if there is a button in form, it will close the modal */}
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
-                        <h3 className="font-bold text-lg">Howdy</h3>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        <h3 className="font-bold text-lg">True U Weight Loss Programs</h3>
+                        <p className="py-4"> <b>1 - Bronze $160/wk ($185/wk for tirzepatide)</b> <br/>o   Semaglutide/tirz injections<br/>o   1 complimentary Inbody assessment <br/>o   10% off lipo injections <br/> <b> 2 - Silver plan $267/wk (+$15 for tirz)</b><br/>o   Everything in Bronze plus…<br/>o   Monthly Inbody assessments<br/>o   Custom nutrition plan<br/>o   24/7 Access to Registered Nurse on WhatsAap<br/>o   10% off IV therapy<br/>o   10% body contouring<br/><b>3 - Gold plan $355/wk (+15 for tirz)</b><br/>o   Everything in silver plus…<br/>o   15% off lipo shots<br/>o   15% off IV therapy<br/>o   10% off RF microneedling facial<br/>o   Weekly Inbody assessment<br/>o   Weekly behavior therapy sessions</p>
                     </div>
                 </dialog>
             </div>
@@ -151,7 +154,7 @@ function Services() {
         </div>
         <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
             <figure className="px-10 pt-10">
-            <img src={LabExam} alt="Lab Exams" className="rounded-xl h-52" />
+            <img src={LabExam} alt="Lab Exams" className="rounded-xl w-72 h-52" />
             </figure>
         <div className="card-body flex-grow-0 items-center text-center">
             <div className="card-actions">
@@ -162,8 +165,8 @@ function Services() {
                             {/* if there is a button in form, it will close the modal */}
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
-                        <h3 className="font-bold text-lg">Shoot!</h3>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        <h3 className="font-bold text-lg">Lab Exams</h3>
+                        <p className="py-4">55 - BMP <br/> 56 - CMP <br/> 57 - Urea <br/> 58 - PSA <br/> 59 - IGF-1 <br/> 60 - SHBG <br/> 61 - LH <br/> 62 - FSH <br/> 63 - Progesterone <br/> 64 - Estradiol <br/> 65 - Estrogen <br/> 66 - Vit D-Labs <br/> 67 - Cortisol <br/> 68 - B12 - Labs <br/> 69 - Ferritin <br/> 70 - Iron-Labs <br/> 71 - Full checkup <br/> 72 - Testosterone-Labs</p>
                     </div>
                 </dialog>
             </div>
@@ -171,7 +174,7 @@ function Services() {
         </div>
         <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
             <figure className="px-10 pt-10">
-            <img src={Inject} alt="Injections" className="rounded-xl" />
+            <img src={Inject} alt="Injections" className="rounded-xl w-72 h-52" />
             </figure>
         <div className="card-body flex-grow-0 items-center text-center">
             <div className="card-actions">
@@ -182,8 +185,8 @@ function Services() {
                             {/* if there is a button in form, it will close the modal */}
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
-                        <h3 className="font-bold text-lg">Dang!</h3>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        <h3 className="font-bold text-lg">Injection Information</h3>
+                        <p className="py-4">1 - Vitamin D $35<br/>2 - Vitamin B12 $35<br/>3 - True U Growth $799 (30 day supply)<br/>4 - Lipo Shots $35<br/>5 - Lipo B Shot $40<br/>6 - Lipo C $45<br/>7 - Gluthatione  $40<br/>8 - Iron IM 189</p>
                     </div>
                 </dialog>
             </div>
@@ -191,7 +194,7 @@ function Services() {
         </div>
         <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
             <figure className="px-10 pt-10">
-            <img src={SkinCare} alt="Skin Care" className="rounded-xl" />
+            <img src={SkinCare} alt="Skin Care" className="rounded-xl w-72 h-52" />
             </figure>
         <div className="card-body flex-grow-0 items-center text-center">
             <div className="card-actions">
@@ -202,8 +205,8 @@ function Services() {
                             {/* if there is a button in form, it will close the modal */}
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
-                        <h3 className="font-bold text-lg">Carp!</h3>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        <h3 className="font-bold text-lg">Skin Care</h3>
+                        <p className="py-4">1 - Infrared sauna blanket $145 3 sessions<br/>2 - Microneedling $280<br/>3 - True hydrofacial $120<br/>4 - Morpheus 8 RF microneedling Full face$650/session (3 sessions $1750)<br/><b>Add-ons</b><br/>PRP $100<br/>HA skin booster $150<br/>Brightening skin booster $150</p>
                     </div>
                 </dialog>
             </div>
@@ -211,7 +214,7 @@ function Services() {
         </div>
         <div className="card hover:ring-2 ring-amber-800/50 w-96 bg-base-100 shadow-2xl">
             <figure className="px-10 pt-10">
-            <img src={WeightLoss} alt="Wellness Weight Loss" className="rounded-xl" />
+            <img src={WeightLoss} alt="Wellness Weight Loss" className="rounded-xl w-72 h-52" />
             </figure>
         <div className="card-body flex-grow-0 items-center text-center">
             <div className="card-actions">
@@ -223,7 +226,7 @@ function Services() {
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                         </form>
                         <h3 className="font-bold text-lg">Finally!</h3>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        <p className="py-4">1 - Consultation + Inbody assessment <br/> 2 - Labs <br/> 3 - MD consultation</p>
                     </div>
                 </dialog>
             </div>
